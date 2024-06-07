c18a1158056febaa1026e0bcf53822aea903ea7f
Jesse-js <jesse-jjs@hotmail.com>
2024-06-06 23:36:25 -0300

    feat: uses real time facades

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,12 @@
 namespace App\Http\Controllers;
 
 
-use App\ConsultaCepInterface;
-use Illuminate\Session\SessionManager;
-use Illuminate\Support\Facades\Session;
+use Facades\App\ConsultaCepInterface;
 
 class HomeController extends Controller
 {
-    public function index(SessionManager $sessionManager, ConsultaCepInterface $consultaCEP)
+    public function index()
     {
-
-        session()->put('test', 'treinaweb');
-        echo session()->get('test');
-        echo '<br/>';
-
-        $sessionManager->put('test', 'treinaweb');
-        echo $sessionManager->get('test');
-        echo '<br/>';
-        
-        Session::put('test', 'treinaweb');
-        echo Session::get('test');
-        echo '<br/>';
-        //$consultaCEP->consultar('01001000');
+        ConsultaCepInterface::consultar('01001000');
     }
 }
