ca842dd2476078e35f56fc7b16fdb0bb0df1595a
Jesse-js <jesse-jjs@hotmail.com>
2024-06-06 23:33:26 -0300

    feat: uses Facades

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers;
 
 
 use App\ConsultaCepInterface;
+use Illuminate\Session\SessionManager;
+use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
 {
-    public function index(ConsultaCepInterface $consultaCEP)
+    public function index(SessionManager $sessionManager, ConsultaCepInterface $consultaCEP)
     {
-        $consultaCEP->consultar('01001000');
+
+        session()->put('test', 'treinaweb');
+        echo session()->get('test');
+        echo '<br/>';
+
+        $sessionManager->put('test', 'treinaweb');
+        echo $sessionManager->get('test');
+        echo '<br/>';
+        
+        Session::put('test', 'treinaweb');
+        echo Session::get('test');
+        echo '<br/>';
+        //$consultaCEP->consultar('01001000');
     }
 }
